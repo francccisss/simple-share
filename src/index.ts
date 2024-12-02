e66c49986fcce3a3f51ebee185b239648b6c5196
francis <franzdiaz460@gmail.com>
2024-12-02 13:20:45 +0400

    ref: I hate testing in yavascript

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { Http2Server } from "http2";
 import routes from "./routes";
 import http from "http";
 import { networkInterfaces } from "os";
+import path from "path";
 
 const app = express();
 const PORT = process.env.PORT || 8081;
 
-app.get("/", routes.index);
+app.use(express.static(path.join(__dirname, "public"), { index: false }));
+
+// create session middleware
+app.get("/", function index(req: Request, res: Response) {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.use("/api", routes);
 
 http.createServer(app).listen(PORT, () => {
   console.log(`Server start: listening to port ${PORT}`);
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,30 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response, Router } from "express";
+import path from "path";
+import multer from "multer";
 
-function index(req: Request, res: Response) {
-  res.send("Welcome to simple search");
-}
+const upload = multer();
 
-export default { index };
+const router = Router();
+
+router.post(
+  "/upload/:sID/",
+  upload.single("file_upload"),
+  function index(req: Request, res: Response) {
+    console.log("Uploaded");
+    console.log(req.file);
+    res.send(`File uploaded by ${req.params.sID}`);
+  },
+);
+
+router.delete(
+  "/remove/:sID/:file",
+  function index(req: Request, res: Response) {
+    res.send(`File removed by user ${req.params.sID}`);
+  },
+);
+
+router.get("/share/:file", function index(req: Request, res: Response) {
+  res.send("File Retrieved");
+});
+
+export default router;
